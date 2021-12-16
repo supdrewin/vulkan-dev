3274abc5bfdc0b3397ac0ae407542b09dbe05459
Supdrewin <supdrewin@outlook.com>
2021-12-16 16:57:53 +0800

    Format

--- tools/template.mk
+++ tools/template.mk
@@ -1,9 +1,10 @@
 # template.mk - supdrewin
 
 .INCLUDE_DIRS += ${workspaceFolder}/include
-CXXFLAGS += -std=c++2a -pedantic
-LDFLAGS  += -L${workspaceFolder}/lib -Wl,-rpath,lib
-LDLIBS += -lvk_swiftshader
+
+CXXFLAGS 			+= -std=c++2a -pedantic
+LDFLAGS  			+= -L${workspaceFolder}/lib -Wl,-rpath,lib
+LDLIBS 				+= -lvk_swiftshader
 
 $(workspaceFolder)/bin/$(PROJECT): *.cpp
 	$(LINK.cpp) $^ $(LOADLIBES) $(LDLIBS) -o $@
