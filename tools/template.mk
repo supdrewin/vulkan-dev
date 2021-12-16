2886c9ffbc22c2176c8e91848e29ed97e53fb870
Supdrewin <supdrewin@outlook.com>
2021-12-16 22:24:30 +0800

    Trim spaces

--- tools/template.mk
+++ tools/template.mk
@@ -1,11 +1,11 @@
 # template.mk - supdrewin
 
-CXXFLAGS   += -pedantic -std=c++2a
-CXXFLAGS   += -I${workspaceFolder}/include
-LDFLAGS    += -L${workspaceFolder}/lib
-LDFLAGS    += -flto -pie
-LDLIBS     += -ldl -lglfw3 -lpthread
-LDLIBS     += -lvulkan
+CXXFLAGS += -pedantic -std=c++2a
+CXXFLAGS += -I${workspaceFolder}/include
+LDFLAGS  += -L${workspaceFolder}/lib
+LDFLAGS  += -flto -pie
+LDLIBS   += -ldl -lglfw3 -lpthread
+LDLIBS   += -lvulkan
 
 output_dir := $(workspaceFolder)/bin
 
