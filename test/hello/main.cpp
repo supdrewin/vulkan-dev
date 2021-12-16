8c7cdb626a78b8279975685c1c10e753d14176d8
Supdrewin <supdrewin@outlook.com>
2021-12-16 18:14:58 +0800

    Improve makefile

---
+++ test/hello/main.cpp
@@ -0,0 +1,7 @@
+#include "GLFW/glfw3.h"
+#include "vulkan/vulkan.hpp"
+
+int main() {
+  //
+  return 0;
+}
--- tools/template.mk
+++ tools/template.mk
@@ -1,11 +1,11 @@
 # template.mk - supdrewin
 
-.INCLUDE_DIRS += ${workspaceFolder}/include
+CXXFLAGS += -std=c++2a -I${workspaceFolder}/include -pedantic
+LDFLAGS  += -L${workspaceFolder}/lib -Wl,-rpath,.
+LDLIBS   += -lglfw3 -lvk_swiftshader
 
-CXXFLAGS      += -std=c++2a -pedantic
-LDFLAGS       += -L${workspaceFolder}/lib -Wl,-rpath,lib
-LDLIBS        += -lglfw3 -lvk_swiftshader
+output_dir := $(workspaceFolder)/bin
 
-$(workspaceFolder)/bin/$(PROJECT): *.cpp
+$(output_dir)/$(PROJECT): *.cpp; mkdir -p $(output_dir)
 	$(LINK.cpp) $^ $(LOADLIBES) $(LDLIBS) -o $@
-.PHONY: $(workspaceFolder)/bin/$(PROJECT)
+.PHONY: $(output_dir)/$(PROJECT)
